c95dc6a8b6ada900e5c21de008c572048c58060b
ayushagarwal2502 <159728868+ayushagarwal2502@users.noreply.github.com>
2025-01-10 01:16:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+let currentUrl = document.URL;
+let urlLength = currentUrl.length;
+
+// Log the URL and its length to the console
+console.log("The URL is: " + currentUrl);
+console.log("The length of the URL is: " + urlLength);
